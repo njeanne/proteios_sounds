6e6f88224db60b5b2de294269b92852e91521298
mesmeraf <mesmer_com@yahoo.fr>
2019-02-10 19:27:53 +0100

    corrected missing keys in protein

--- parse_uniprot.py
+++ parse_uniprot.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python3
 
-import os
 import sys
 import re
 import urllib
@@ -8,20 +7,27 @@ from Bio import ExPASy
 from Bio import SwissProt
 from Bio import SeqIO
 
-def parse_entry(uniprot_accession_number, out_dir, logger):
-
+def parse_entry(uniprot_AN, out_dir, logger):
+    '''
+    Parse the uniprot entry.
+    :param int uniprot_AN: the uniprot accession number for the protein
+    :param str out_dir: the path to the output directory
+    :param logger logger: the logger
+    :return: the dictionary describing the protein
+    :rtype: dict
+    '''
     ### Load the Uniprot entry
-    # uniprot_file_path = os.path.join(out_dir, '{}.dat'.format(uniprot_accession_number))
+    # uniprot_file_path = os.path.join(out_dir, '{}.dat'.format(uniprot_AN))
     try:
-        handle = ExPASy.get_sprot_raw(uniprot_accession_number)
+        handle = ExPASy.get_sprot_raw(uniprot_AN)
         uniprot = SwissProt.read(handle)
-        print(uniprot)
     except urllib.error.HTTPError as err:
-        msg = '{}: {} accession number does not exists in UniProt database, check https://www.uniprot.org/ for the correct accession number.'.format(err, uniprot_accession_number)
+        msg = '{}: {} accession number does not exists in UniProt database, check https://www.uniprot.org/ for the correct accession number.'.format(err, uniprot_AN)
         print('ERROR: {}'.format(msg))
         logger.error(msg)
         sys.exit(1)
-    #TODO: coder erreur uniprot indisponible
+    #TODO:
+    #coder erreur uniprot indisponible
     # except urllib.error.ATROUVER:
     #     msg = 'Uniprot web site unavailable (https://www.uniprot.org/)'
     #     if not os.path.exists(uniprot_file_path):
@@ -37,7 +43,7 @@ def parse_entry(uniprot_accession_number, out_dir, logger):
     # get the organism and entry name in UniProt
     organism = uniprot.organism
     if '(' in organism:
-         organism = organism.split('(')[1].split(')')[0]
+        organism = organism.split('(')[1].split(')')[0]
     organism = organism.rstrip('.,;:').replace(' ', '_')
     entry_name = uniprot.entry_name
     uniprot_sequence = uniprot.sequence
@@ -48,9 +54,9 @@ def parse_entry(uniprot_accession_number, out_dir, logger):
     # create a dictionary to get the structures positions => type
     structures = {}
     # parse the UniProt Features (see https://www.uniprot.org/help/?query=*&fil=category%3A%22PTM+processing%22+AND+section%3Amanual&columns=title)
-    for feature in uniprot.features:
-        print(feature)
-    print('###########################################\n\n')
+    # for feature in uniprot.features:
+    #     print(feature)
+    # print('###########################################\n\n')
     for feature in uniprot.features:
         # strucutre information
         if feature[0] == 'HELIX' or feature[0] == 'STRAND' or feature[0] == 'TURN':
@@ -60,11 +66,11 @@ def parse_entry(uniprot_accession_number, out_dir, logger):
             if 'signal_peptide' in protein.keys():
                 protein['signal_peptide'].append((feature[1], feature[2]))
             else:
-                protein['signal_peptide']= [(feature[1], feature[2])]
+                protein['signal_peptide'] = [(feature[1], feature[2])]
         # Modified residue: Modified residues excluding lipids, glycans and protein crosslinks
         if feature[0] == 'MOD_RES':
             modif = re.split(' {', feature[3])[0].rstrip('.;,')
-            if not 'modified_residue' in protein.keys():
+            if 'modified_residue' not in protein.keys():
                 protein['modified_residue'] = {modif: [(feature[1], feature[2])]}
             else:
                 if modif in protein['modified_residue'].keys():
@@ -74,7 +80,7 @@ def parse_entry(uniprot_accession_number, out_dir, logger):
         # Glycosylation: Covalently attached glycan group(s)
         if feature[0] == 'CARBOHYD':
             glyco = re.split(' ', feature[3])[0].rstrip('.;,')
-            if not 'glycosylation' in protein.keys():
+            if 'glycosylation' not in protein.keys():
                 protein['glycosylation'] = {glyco: [(feature[1], feature[2])]}
             else:
                 if glyco in protein['glycosylation'].keys():
@@ -84,18 +90,23 @@ def parse_entry(uniprot_accession_number, out_dir, logger):
         # Site: Any interesting single amino acid site on the sequence
         if feature[0] == 'SITE':
             site = re.split(' {', feature[3])[0].rstrip('.;,')
-            print('SITE: {}'.format(site))
-            if site in protein['site'].keys():
-                protein['site'][site].append((feature[1], feature[2]))
+            if 'site' not in protein:
+                protein['site'] = {site: [(feature[1], feature[2])]}
             else:
-                protein['site'][site] = [(feature[1], feature[2])]
+                if site in protein['site']:
+                    protein['site'][site].append((feature[1], feature[2]))
+                else:
+                    protein['site'][site] = [(feature[1], feature[2])]
         # Propeptide: Part of a protein that is cleaved during maturation or activation
         if feature[0] == 'PROPEP':
             propep = re.split(' {', feature[3])[0].rstrip('.;,')
-            if propep in protein['propeptide'].keys():
-                protein['propeptide'][propep].append((feature[1], feature[2]))
+            if 'propeptide' not in protein:
+                protein['propeptide'] = {propep: [(feature[1], feature[2])]}
             else:
-                protein['propeptide'][propep] = [(feature[1], feature[2])]
+                if propep in protein['propeptide'].keys():
+                    protein['propeptide'][propep].append((feature[1], feature[2]))
+                else:
+                    protein['propeptide'][propep] = [(feature[1], feature[2])]
         # Disulfid bond, get the two linked positions on the same chain (no interchain)
         if feature[0] == 'DISULFID':
             if not feature[3].startswith('Interchain'):
@@ -128,45 +139,45 @@ def parse_entry(uniprot_accession_number, out_dir, logger):
         else:
             protein['structure'] = {structure_last_position + 1: 'FREE'}
 
-    ### TOREMOVE
-    print('#####################################################\n\n')
-    print('structures :')
-    for structure in structures:
-        print(structure)
-    print('#####################################################\n\n')
-    for k,v in protein.items():
-        if k == 'modified_residue':
-            print(k)
-            for k2,v2 in v.items():
-                print('\t{}'.format(k2))
-                for pos in v2:
-                    print('\t\t{}'.format(pos))
-        elif k == 'glycosylation':
-            print(k)
-            for k2,v2 in v.items():
-                print('\t{}'.format(k2))
-                for pos in v2:
-                    print('\t\t{}'.format(pos))
-        elif k == 'site':
-            print(k)
-            for k2,v2 in v.items():
-                print('\t{}'.format(k2))
-                for pos in v2:
-                    print('\t\t{}'.format(pos))
-        elif k == 'propeptide':
-            print(k)
-            for k2,v2 in v.items():
-                print('\t{}'.format(k2))
-                for pos in v2:
-                    print('\t\t{}'.format(pos))
-        elif k == 'disulfid':
-            print(k)
-            for bond in v:
-                print('\t{} disulfid bond to {}'.format(bond[0], bond[1]))
-        elif k == 'signal_peptide':
-            print(k)
-            for signal_peptide in v:
-                print('\tsignal peptide from {} to {}'.format(signal_peptide[0], signal_peptide[1]))
-    #################
+    # ### TOREMOVE
+    # print('#####################################################\n\n')
+    # print('structures :')
+    # for structure in structures:
+    #     print(structure)
+    # print('#####################################################\n\n')
+    # for k, v in protein.items():
+    #     if k == 'modified_residue':
+    #         print(k)
+    #         for k2, v2 in v.items():
+    #             print('\t{}'.format(k2))
+    #             for pos in v2:
+    #                 print('\t\t{}'.format(pos))
+    #     elif k == 'glycosylation':
+    #         print(k)
+    #         for k2, v2 in v.items():
+    #             print('\t{}'.format(k2))
+    #             for pos in v2:
+    #                 print('\t\t{}'.format(pos))
+    #     elif k == 'site':
+    #         print(k)
+    #         for k2, v2 in v.items():
+    #             print('\t{}'.format(k2))
+    #             for pos in v2:
+    #                 print('\t\t{}'.format(pos))
+    #     elif k == 'propeptide':
+    #         print(k)
+    #         for k2, v2 in v.items():
+    #             print('\t{}'.format(k2))
+    #             for pos in v2:
+    #                 print('\t\t{}'.format(pos))
+    #     elif k == 'disulfid':
+    #         print(k)
+    #         for bond in v:
+    #             print('\t{} disulfid bond to {}'.format(bond[0], bond[1]))
+    #     elif k == 'signal_peptide':
+    #         print(k)
+    #         for signal_peptide in v:
+    #             print('\tsignal peptide from {} to {}'.format(signal_peptide[0], signal_peptide[1]))
+    # #################
 
     return protein
