26e6a5c7e9eb942467d50a5ac0f9a10a246fbe58
mesmeraf <mesmer_com@yahoo.fr>
2019-06-26 12:37:58 +0200

    add video frames

--- protein_movie.py
+++ protein_movie.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 
+import os
+import numpy as np
 import cv2
 from cv2 import VideoWriter, VideoWriter_fourcc
 
@@ -13,8 +15,15 @@ def create_movie(path_movie, dir_frames, duration_keys, logger):
     :return: the movie path.
     :rtype: str
     '''
-
-    print('[protein_movie.py]: {}'.format(duration_keys))
+    # create a dictionary of the frames directory with the index as keys
+    frames_dict = {}
+    for png in os.listdir(dir_frames):
+        idx_frame_in_prot = os.path.splitext(png)[0].split('_')[1]
+        frames_dict[idx_frame_in_prot] = cv2.imread(os.path.join(dir_frames,
+                                                                 png),
+                                                    1)
+    print('[protein_movie.py]: {}'.format(frames_dict))
+    # set the video parameters
     width = 1280
     height = 720
     FPS = 24
@@ -24,5 +33,16 @@ def create_movie(path_movie, dir_frames, duration_keys, logger):
     # set the video writer
     video = VideoWriter(path_movie, fourcc, float(FPS), (width, height))
     # create the frames per second
-    # for idx, key_duration in enumerate(duration_keys):
-    #     video.write()
+    for idx, key_duration in enumerate(duration_keys):
+        nb_frames_on_key = int(FPS * key_duration)
+        print('[protein_movie.py]: key duration: {}\t nb frames: {}'.format(key_duration,
+                                                        nb_frames_on_key))
+        if str(idx) in frames_dict:
+            for _ in range(nb_frames_on_key):
+                video.write(frames_dict[str(idx)])
+        else:
+            for _ in range(nb_frames_on_key):
+                video.write(frames_dict['no-idx'])
+
+
+    video.release()
